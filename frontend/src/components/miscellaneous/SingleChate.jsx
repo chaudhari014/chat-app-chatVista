d5bda18d78b00d5f6bbb126d6e66b46d95bd0c5e
chaudhari014 <fatarambhai.k14@gmail.com>
2023-10-05 04:13:52 +0530

    style change

--- frontend/src/components/miscellaneous/SingleChate.jsx
+++ frontend/src/components/miscellaneous/SingleChate.jsx
@@ -181,7 +181,6 @@ const SingleChate = ({ fetchAgain, setFetchAgain }) => {
             w="100%"
             h="100%"
             borderRadius="lg"
-            border={"1px solid red"}
             overflowY="hidden"
           >
             {loading ? (
